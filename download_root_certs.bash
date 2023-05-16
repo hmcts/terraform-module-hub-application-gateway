88a898179567411f3f17334eb4d4858f93b6ba1d
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-16 09:33:46 +0100

    changed permission

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#grabing the public root CA
+#grabing the public root CA 
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
-#grabing the public intermediate CA 
+#grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 
 cat root.pem intermediate.pem > merged.pem
