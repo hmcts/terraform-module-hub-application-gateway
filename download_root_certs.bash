eb382a236598e10805ff8b264d33b71bfa31bacd
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-16 17:13:07 +0100

    updated bash file for it run

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#grabing the public root CA
+#grabing the public root CA 
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
