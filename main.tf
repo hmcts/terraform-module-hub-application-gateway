492710d8d5d3459a47fc620f3bdc41e02f982a40
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 08:35:23 +0100

    added path to module

--- main.tf
+++ main.tf
@@ -223,7 +223,7 @@ resource "azurerm_application_gateway" "ag" {
     ]
     content {
       name = trusted_client_certificate.value.name
-      data = file("./merged.pem")
+      data = file("${path.module}/merged.pem")
     }
   }
 
