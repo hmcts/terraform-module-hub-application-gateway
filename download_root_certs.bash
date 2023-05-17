7f19f5d1bbeaec3c0742d6cd45da5955d4b0bcc9
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 08:59:08 +0100

    changed path

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+cd .terraform/modules/app-gw
 #grabing the public root CA 
+ls
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
