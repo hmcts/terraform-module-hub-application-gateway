b6f9cbe16379d1f36b0dd28f0ddd71fe00985d17
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 09:39:32 +0100

    checking merged file

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -8,5 +8,6 @@ curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 
 cat root.pem intermediate.pem >merged.pem
+cat merged.pem
 ls
 pwd
