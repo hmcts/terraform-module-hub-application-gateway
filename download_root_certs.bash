0bb8c4136ab6e94260ecd90d0326f9a3d030d2c7
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 08:48:11 +0100

    getting file from the root folder

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+
 #grabing the public root CA 
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 pwd
 ls
-cat root.pem intermediate.pem > merged.pem
+cat root.pem intermediate.pem >merged.pem
 ls
 pwd
