708350efa852d75036f36e6f5a86c44b4bf35227
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 08:11:59 +0100

    checking file path

--- download_root_certs.bash
+++ download_root_certs.bash
@@ -3,5 +3,6 @@
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
-
+pwd
+ls
 cat root.pem intermediate.pem > merged.pem
