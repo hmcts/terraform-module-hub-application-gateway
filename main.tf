a89152ec7e985915900ff07f7bc6a32f2f3eff2b
Daniel Wilson <daniel.wilson@kainos.com>
2024-06-17 13:54:51 +0100

    iterate trusted cert

--- main.tf
+++ main.tf
@@ -242,10 +242,12 @@ resource "azurerm_application_gateway" "ag" {
   }
 
   dynamic "trusted_client_certificate" {
-    for_each = [for app in local.gateways[count.index].app_configuration : {
-      name                         = "${app.product}-${app.component}-trusted-cert"
+    for_each = [
+      for app in local.gateways[count.index].app_configuration :
+      for cert in app.ssl_profile_certificates : {
+      name                         = "${app.product}-${app.component}-${cert.rootca_certificate_name}"
       verify_client_cert_issuer_dn = contains(keys(app), "verify_client_cert_issuer_dn") ? app.verify_client_cert_issuer_dn : false
-      data                         = contains(keys(app), "rootca_certificate_name") ? var.trusted_client_certificate_data[app.rootca_certificate_name].path : false
+      data                         = contains(keys(cert), "rootca_certificate_name") ? var.trusted_client_certificate_data[cert.rootca_certificate_name].path : false
       }
       if lookup(app, "add_ssl_profile", false) == true
     ]
